227b4bae020c68430a5d96882f31e76ae84db2fa
chuy2008 <chuylozano@ieee.org>
2014-07-15 13:03:23 -0500

    init

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,11 +15,13 @@ object Application extends Controller
   def receiveJson1 = Action(parse.json)
   {
       request => 
-         println("Application scala routine, line 18, request content type = " + request.contentType)
-         println("Application scala routine, line 19, request body = " + request.body)
-         println("Application scala routine, line 20, request body to String = " + request.toString)
+         println("Application scala routine, line 18, request method type = " + request.method)
+         println("Application scala routine, line 19, request content type = " + request.contentType)
+         println("Application scala routine, line 20, request body = " + request.body)
+         println("Application scala routine, line 21, request body to String = " + request.body.toString)
          var cel = ""
          var code = ""
+         var infoIncomingRequest = ""
          (request.body \ "Cel").asOpt[String].map { cell => cel = cell}.getOrElse 
          {
              BadRequest("Missing parameter [name]")
@@ -27,19 +29,26 @@ object Application extends Controller
          (request.body \ "Code").asOpt[String].map { codd => code = codd}.getOrElse 
          {
              BadRequest("Missing parameter [name]")
-         }         
+         }
+         infoIncomingRequest = "method = " + request.method + "\r\n" + 
+                               "content type = " + request.contentType + "\r\n" + 
+                               "body" + request.body.toString                              
          System.out.println("Application scala routine, line 29, cel received =  " + cel)
          System.out.println("Application scala routine, line 27, code received =  " + code)        
-         Ok(Json.toJson(Map("Cel" -> cel, "Code" -> code )))
+         Ok(Json.toJson(Map("Cel" -> cel, "Code" -> code, "InfoIncomingRequest" -> infoIncomingRequest )))
   }
 
   def receiveSimple1 = Action
   {
       request => 
-         println("Application scala routine, line 35, request content type = " + request.contentType)
-         println("Application scala routine, line 36, request body = " + request.body)
-         println("Application scala routine, line 37, request body to String = " + request.toString)
-         Ok("received simple stuff" + request.toString + "body..." + request.body.toString)
+         println("Application scala routine, line 39, request method type = " + request.method)
+         println("Application scala routine, line 40, request content type = " + request.contentType)
+         println("Application scala routine, line 41, request body = " + request.body)
+         println("Application scala routine, line 42, request to String = " + request.toString)
+         Ok("received method = " + request.method +
+            "received content type = " + request.contentType + 
+            "received simple stuff = " + request.toString + 
+            "body = " + request.body.toString)
   }  
   
   
